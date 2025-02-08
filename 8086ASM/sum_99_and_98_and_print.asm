702e9ca05e42b58cf1553d0e77462dbe88c2287b
Kazi Rifat Morshed <kazirifat03.krm@gmail.com>
2025-02-08 15:51:44 +0600

    done peinting 3 digit sum

---
+++ 8086ASM/sum_99_and_98_and_print.asm
@@ -0,0 +1,54 @@
+.model small
+.stack 100h
+
+.data
+a dw 99
+b dw 98
+sum dw 0
+rem db 0
+
+.code
+main proc
+mov ax, @data
+mov ds, ax
+
+; do sum
+mov ax, a
+add ax, b ;; now AX holds the sum
+; store sum
+mov sum, ax
+
+; process first digit
+mov bl, 100 ; for sake of division
+div bl
+mov rem, ah
+;;;;;;;;;; mov bh, al ;; TEMPORARELY STORING VAG FOL TO BH
+mov dl, al
+add dl, 48
+mov ah, 2
+int 21h ;; first digit print done
+
+; process 2nd digit
+;; CAN NOT DO "mov ax, rem"
+mov ah, 0
+mov al, rem
+mov bl, 10
+div bl
+mov rem, ah
+mov dl, al
+add dl, 48 ;; FORGOT
+mov ah, 2
+int 21h
+
+; process last (right-most) digit
+mov dl, rem
+add dl, 48 ;; FORGOT
+mov ah, 2
+int 21h
+
+mov ax, 4c00h
+int 21h
+
+main endp
+end main
+;; done
---
+++ 8086ASM/sum_9_and_50_and_print.asm
@@ -0,0 +1,40 @@
+.model small
+.stack 100h
+
+.data
+a dw 9
+b dw 50
+sum dw 0
+rem db 0
+
+.code
+main proc
+mov ax, @data
+mov ds, ax
+
+mov ax, a
+add ax, b
+mov sum, ax
+
+mov ah, 0 ;; careful
+;div 10 ;; can not divide a reg with direct value
+mov bh, 10
+div bh
+mov rem, ah
+mov dl, al
+add dl, 48
+mov ah, 2
+int 21h
+
+mov ah, 0
+mov dl, rem ;; reminder ke DL e nibo print korar jonno
+add dl, 48
+mov ah, 2
+int 21h
+
+MOV AX,4C00H
+INT 21H
+
+main endp
+end main
+;; done
